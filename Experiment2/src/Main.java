5f6d2f8550f51dd3c25b36a9cf702ad0ded7ed25
Meko Deng <mekodeng94@gmail.com>
2018-10-02 14:38:10 -0400

    ignore commit

--- Experiment2/src/Main.java
+++ Experiment2/src/Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        for (int i=0; i<args.length;i++){
+            System.out.println(args[i]);
+        }
     }
 }
