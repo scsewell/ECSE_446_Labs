f8b17c5861671f0b9c6ffe9896148c84cb6f849c
Meko Deng <mekodeng94@gmail.com>
2018-10-09 13:43:24 -0400

    updated packet_interpretation

--- Experiment2/src/Packet_Interpreting.java
+++ Experiment2/src/Packet_Interpreting.java
@@ -3,6 +3,9 @@ public class Packet_Interpreting {
     public static boolean AA;       //1: name server is authority for a domain name in question section
     public static String error;
     public static boolean isTruncated;
+    public static String isRecursiveSupported;
+    public static String RCODE_message;
+    public static int ANCOUNT;
 
     public static void main(String[] args, byte[] received_packet) {
         //Question: Should we interpret the packet Header and packet Questions as well?
@@ -25,9 +28,35 @@ public class Packet_Interpreting {
 
         //retrieve isTruncated
         isTruncated = (response[2] >> 2 & 1) == 1;
+
+        //retrieve RA
+        set_RecursiveMessage((response[3] >> 7 & 1 ) == 1);
+
+        //retrieve RCODE
+        int RCODE = response[3] & 0b00001111;
+
+
+        //retrieve ANCOUNT
+
     }
 
     public static boolean isResponse(byte[] response){
         return (response[2] >> 7 & 1) == 1;
+    }
+
+    public static void set_RecursiveMessage(boolean isRecursive){
+        if (isRecursive){
+            isRecursiveSupported = "Recursive queries are supported";
+        }else {
+            isRecursiveSupported = "Recursive queries are not supported";
+        }
+    }
+
+    public static void set_RCODE_message(int code){
+        if (code == 3){
+            RCODE_message = "NOTFOUND";
+        } else if (code != 0) {
+            RCODE_message = "ERROR";
+        }
     }
 }
